77fc86cfc5065d6713a2e09b59e8bef5cd6b4b65
Owen Senowitz <owen.senowitz@gmail.com>
2023-11-27 20:01:45 -0500

    added dynamic scroll buttons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
-import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
-import { useState } from "react";
 
 const Nav = () => {
-	const [activeNav, setActiveNav] = useState("#");
-	return (
-		<nav>
-			<a
-				href="#"
-				onClick={() => setActiveNav("#")}
-				className={activeNav === "#" ? "active" : ""}
-			>
-				<AiOutlineHome />
-			</a>
-			<a
-				href="#about"
-				onClick={() => setActiveNav("#about")}
-				className={activeNav === "#about" ? "active" : ""}
-			>
-				<AiOutlineUser />
-			</a>
-			<a
-				href="#experience"
-				onClick={() => setActiveNav("#experience")}
-				className={activeNav === "#experience" ? "active" : ""}
-			>
-				<BiBook />
-			</a>
-			<a
-				href="#contact"
-				onClick={() => setActiveNav("#contact")}
-				className={activeNav === "#contact" ? "active" : ""}
-			>
-				<BiMessageSquareDetail />
-			</a>
-		</nav>
-	);
+  const [activeNav, setActiveNav] = useState("#");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const sections = document.querySelectorAll("section");
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionId = `#${section.id}`;
+
+        if (scrollPosition >= sectionTop) {
+          setActiveNav(sectionId);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav>
+      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}>
+        <AiOutlineHome />
+      </a>
+      <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}>
+        <AiOutlineUser />
+      </a>
+      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}>
+        <BiBook />
+      </a>
+      <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}>
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
